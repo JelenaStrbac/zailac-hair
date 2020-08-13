e47d7e6a9deb7db4b9af46e328cfb59851edbb8f
apple juice <strbac.jelena.js@gmail.com>
2020-08-13 11:31:06 +0200

    initialization

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,12 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `zailac-hair`,
+    description: `Zailac hair frizerski salon website.`,
+    author: `@JelenaStrbac`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -26,6 +30,54 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
+    },
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        baseUrl: `example23340803.wordpress.com`,
+        protocol: `https`,
+        restApiRoutePrefix: "wp-json",
+        hostingWPCOM: true,
+        auth: {
+          wpcom_app_clientSecret: process.env.GATSBY_WORDPRESS_CLIENT_SECRET,
+          wpcom_app_clientId: process.env.GATSBY_CLIENT_ID,
+          wpcom_user: process.env.GATSBY_USER,
+          wpcom_pass: process.env.GATSBY_WORDPRESS_PASSWORD,
+        },
+        includedRoutes: [
+          "**/categories",
+          "**/posts",
+          "**/pages",
+          "**/media",
+          "**/tags",
+          "**/taxonomies",
+          "**/users",
+        ],
+        excludedRoutes: [
+          "/wp/v2/users/**",
+          "/wp/v2/settings*",
+          "/wp/v2/themes*",
+        ],
+      },
+    },
+    "gatsby-plugin-styled-components",
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Crimson Pro`,
+            variants: [`200`, `300`, `400`, `500`],
+          },
+          {
+            family: `Lato`,
+            variants: [`100`, `300`, `400`],
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
