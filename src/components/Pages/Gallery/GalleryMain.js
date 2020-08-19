d03362bc358b04b5d0552f97706f1b1f27b1c1fd
apple juice <strbac.jelena.js@gmail.com>
2020-08-19 12:19:12 +0200

    [feat]: gallery test

--- src/components/Pages/Gallery/GalleryMain.js
+++ src/components/Pages/Gallery/GalleryMain.js
@@ -8,6 +8,14 @@ import One from "../../../images/01.jpg"
 import Two from "../../../images/02.jpg"
 import Three from "../../../images/03.jpg"
 import Four from "../../../images/04.jpg"
+import Five from "../../../images/05.jpg"
+import Six from "../../../images/06.jpg"
+import Seven from "../../../images/07.jpg"
+import Eight from "../../../images/08.jpg"
+import Nine from "../../../images/09.jpg"
+import Ten from "../../../images/10.jpg"
+import Eleven from "../../../images/11.jpg"
+import Twelve from "../../../images/12.jpg"
 
 const GalleryMain = props => {
   return (
@@ -18,14 +26,14 @@ const GalleryMain = props => {
         <GalleryCard image={Two} />
         <GalleryCard image={Three} />
         <GalleryCard image={Four} />
-        <GalleryCard image={One} />
-        <GalleryCard image={Two} />
-        <GalleryCard image={Three} />
-        <GalleryCard image={Four} />
-        <GalleryCard image={One} />
-        <GalleryCard image={Two} />
-        <GalleryCard image={Three} />
-        <GalleryCard image={Four} />
+        <GalleryCard image={Five} />
+        <GalleryCard image={Six} />
+        <GalleryCard image={Seven} />
+        <GalleryCard image={Eight} />
+        <GalleryCard image={Nine} />
+        <GalleryCard image={Ten} />
+        <GalleryCard image={Eleven} />
+        <GalleryCard image={Twelve} />
         <GalleryCard image={One} />
         <GalleryCard image={Two} />
         <GalleryCard image={Three} />
