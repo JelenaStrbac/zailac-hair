1ac429bae391f6323962045ce3e45af85a5e48ec
apple juice <strbac.jelena.js@gmail.com>
2020-08-25 16:15:27 +0200

    [fix]: btn style

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -3,7 +3,11 @@ import React from "react"
 import styles from "./Button.module.css"
 
 const Button = props => {
-  return <button className={styles.btn}>{props.children}</button>
+  return (
+    <div className={styles.btnOutter}>
+      <button className={styles.btn}>{props.children}</button>
+    </div>
+  )
 }
 
 export default Button
