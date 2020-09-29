7b2e8b7d67722b77b0abf728af71b1c0c174d69a
apple juice <strbac.jelena.js@gmail.com>
2020-09-29 11:36:57 +0200

    [feat]: add redirects from netlify subdomain

--- gatsby-config.js
+++ gatsby-config.js
@@ -166,7 +166,7 @@ module.exports = {
         trackingId: "UA-178266974-1",
       },
     },
-
+    `gatsby-plugin-netlify`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
